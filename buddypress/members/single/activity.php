6c8b152f4730168770002fb0bb4698c9fa5e86fc
karmatosed <karmatosed@gmail.com>
2013-07-28 15:36:53 -0700

    Member activity

--- buddypress/members/single/activity.php
+++ buddypress/members/single/activity.php
@@ -6,11 +6,16 @@
  * @subpackage Templatepack
  */
 ?>
-<?php
-//if ( is_user_logged_in() && bp_is_my_profile() && ( !bp_current_action() || bp_is_current_action( 'just-me' ) ) )
-//	bp_get_template_part( 'activity/post-form' );
-?>
-
 <div id="member-activity">
 	<?php bp_get_template_part( 'activity/activity-loop' ) ?>
 </div>
+<?php do_action( 'bp_before_member_activity_post_form' ); ?>
+<?php
+/**if ( is_user_logged_in() && bp_is_my_profile() && ( !bp_current_action() || bp_is_current_action( 'just-me' ) ) )
+	bp_get_template_part( 'activity/post-form' );**/
+do_action( 'bp_after_member_activity_post_form' );
+do_action( 'bp_before_member_activity_content' ); ?>
+<div id="member-activity">
+	<?php bp_get_template_part( 'activity/activity-loop' ) ?>
+</div>
+<?php do_action( 'bp_after_member_activity_content' ); ?>
