40043e611b777ece19537cf80bfc22de0334d413
Noel Tock <noel@humanmade.co.uk>
2013-07-28 13:57:15 -0700

    Add Members Page

--- buddypress/members/single/messages.php
+++ buddypress/members/single/messages.php
@@ -6,4 +6,23 @@
  * @subpackage Templatepack
  */
 ?>
-<!-- to come -->
+<div class="messages-wrap">
+
+	<div class="messages-list">
+		<ul>
+			<li><a href="http://"><?php _e( 'From', 'buddypress' ); ?> Username</a><br>Message Title</li>
+			<li><a href="http://"><?php _e( 'From', 'buddypress' ); ?> Username</a><br>Message Title</li>
+			<li><a href="http://"><?php _e( 'From', 'buddypress' ); ?> Username</a><br>Message Title</li>
+			<li><a href="http://"><?php _e( 'From', 'buddypress' ); ?> Username</a><br>Message Title</li>
+			<li><a href="http://"><?php _e( 'From', 'buddypress' ); ?> Username</a><br>Message Title</li>
+		</ul>
+	</div>
+	<div class="messages-content-wrap">
+		<h3>Message Title</h3>
+		<div class="message-content">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+		</div>
+	</div>
+
+</div>
