9c882398f9b24a0e02c678b906bf7688c3d50dfa
karmatosed <karmatosed@gmail.com>
2013-07-28 14:44:24 -0700

    Groups loop set

--- buddypress/groups/index.php
+++ buddypress/groups/index.php
@@ -6,4 +6,19 @@
  * @subpackage Templatepack
  */
 ?>
-<!-- to come -->
+<?php do_action( 'bp_before_directory_groups_page' ); ?>
+<div id="buddypress">
+	<?php do_action( 'bp_before_directory_groups' ); ?>
+	<?php do_action( 'bp_before_directory_groups_content' ); ?>
+	<form action="" method="post" id="groups-directory-form" class="dir-form">
+		<?php do_action( 'template_notices' ); ?>
+		<div id="group-directory" class="directory-list">
+			<?php bp_get_template_part( 'groups/groups-loop' ); ?>
+		</div>
+		<?php do_action( 'bp_directory_groups_content' ); ?>
+		<?php wp_nonce_field( 'directory_groups', '_wpnonce-groups-filter' ); ?>
+		<?php do_action( 'bp_after_directory_groups_content' ); ?>
+	</form>
+	<?php do_action( 'bp_after_directory_groups' ); ?>
+</div>
+<?php do_action( 'bp_after_directory_groups_page' ); ?>
