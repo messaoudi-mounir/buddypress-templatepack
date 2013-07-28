5df15e7c215d3093278c5c3b2cb259ea844700e6
karmatosed <karmatosed@gmail.com>
2013-07-28 16:29:27 -0700

    Closing comment

--- buddypress/groups/index.php
+++ buddypress/groups/index.php
@@ -12,13 +12,13 @@
 	<?php do_action( 'bp_before_directory_groups_content' ); ?>
 	<form action="" method="post" id="groups-directory-form" class="directory-form">
 		<?php do_action( 'template_notices' ); ?>
-		<div id="group-directory" class="directory-list">
+		<div id="groups-directory" class="directory-list">
 			<?php bp_get_template_part( 'groups/groups-loop' ); ?>
 		</div><!-- end #group-directory -->
 		<?php do_action( 'bp_directory_groups_content' ); ?>
 		<?php wp_nonce_field( 'directory_groups', '_wpnonce-groups-filter' ); ?>
 		<?php do_action( 'bp_after_directory_groups_content' ); ?>
-	</form>
+	</form><!-- end #groups-directory-form -->
 	<?php do_action( 'bp_after_directory_groups' ); ?>
 </div><!-- end #buddypress -->
 <?php do_action( 'bp_after_directory_groups_page' ); ?>
