ab0052114b02be8ee12574fdfaa0555f116b6533
karmatosed <karmatosed@gmail.com>
2013-08-12 19:25:41 +0100

    Group template

--- buddypress/groups/single/group-header.php
+++ buddypress/groups/single/group-header.php
@@ -6,4 +6,70 @@
  * @subpackage Templatepack
  */
 ?>
-<!-- to come -->
+
+<div id="group-header">
+<?php
+
+do_action( 'bp_before_group_header' );
+
+?>
+	
+	<div id="group-actions">
+
+	<?php if ( bp_group_is_visible() ) : ?>
+
+		<h3><?php _e( 'Group Admins', 'buddypress' ); ?></h3>
+
+		<?php bp_group_list_admins();
+
+		do_action( 'bp_after_group_menu_admins' );
+
+		if ( bp_group_has_moderators() ) :
+			do_action( 'bp_before_group_menu_mods' ); ?>
+
+			<h3><?php _e( 'Group Mods' , 'buddypress' ); ?></h3>
+
+			<?php bp_group_list_mods();
+
+			do_action( 'bp_after_group_menu_mods' );
+
+		endif;
+
+	endif; ?>
+
+</div><!-- #item-actions -->
+
+<div id="group-header-avatar">
+	<a href="<?php bp_group_permalink(); ?>" title="<?php bp_group_name(); ?>">
+
+		<?php bp_group_avatar(); ?>
+
+	</a>
+</div><!-- #item-header-avatar -->
+
+<div id="group-header-content">
+	<span class="highlight"><?php bp_group_type(); ?></span>
+	<span class="activity"><?php printf( __( 'active %s', 'buddypress' ), bp_get_group_last_active() ); ?></span>
+
+	<?php do_action( 'bp_before_group_header_meta' ); ?>
+
+	<div id="group-meta">
+
+		<?php bp_group_description(); ?>
+
+		<div id="group-buttons">
+
+			<?php do_action( 'bp_group_header_actions' ); ?>
+
+		</div><!-- #item-buttons -->
+
+		<?php do_action( 'bp_group_header_meta' ); ?>
+
+	</div>
+</div><!-- #item-header-content -->
+
+<?php
+do_action( 'bp_after_group_header' );
+do_action( 'template_notices' );
+?>
+</div>
--- buddypress/groups/single/plugins.php
+++ buddypress/groups/single/plugins.php
@@ -6,4 +6,16 @@
  * @subpackage Templatepack
  */
 ?>
-<!-- to come -->
+<?php do_action( 'bp_before_group_plugin_template' ); ?>
+
+<div id="plugins-content">
+
+	<?php do_action( 'bp_before_group_body' ); ?>
+
+	<?php do_action( 'bp_template_content' ); ?>
+
+	<?php do_action( 'bp_after_group_body' ); ?>
+
+</div><!-- #item-body -->
+
+<?php do_action( 'bp_after_group_plugin_template' );
