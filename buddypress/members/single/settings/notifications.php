f6513bed345b678624b9656eec0596f386c0151c
karmatosed <karmatosed@gmail.com>
2013-08-26 21:36:33 +0100

    Member settings

--- buddypress/members/single/settings/notifications.php
+++ buddypress/members/single/settings/notifications.php
@@ -6,4 +6,22 @@
  * @subpackage Templatepack
  */
 ?>
-<!-- to come -->
+<?php do_action( 'bp_before_member_settings_template' ); ?>
+
+<form action="<?php echo bp_displayed_user_domain() . bp_get_settings_slug() . '/notifications'; ?>" method="post" class="buddypress-form" id="settings-form">
+	<p><?php _e( 'Send a notification by email when:', 'buddypress' ); ?></p>
+
+	<?php do_action( 'bp_notification_settings' ); ?>
+
+	<?php do_action( 'bp_members_notification_settings_before_submit' ); ?>
+
+	<input type="submit" name="submit" value="<?php _e( 'Save Changes', 'buddypress' ); ?>" id="submit" class="auto" />
+	
+	<?php do_action( 'bp_members_notification_settings_after_submit' ); ?>
+
+	<?php wp_nonce_field('bp_settings_notifications' ); ?>
+
+</form>
+
+<?php do_action( 'bp_after_member_settings_template' ); ?>
+
