04b0017758eff7ea0ea42b9985749e4ea23e45e8
karmatosed <karmatosed@gmail.com>
2013-09-01 22:51:27 +0100

    Group directory

--- buddypress/groups/groups-loop.php
+++ buddypress/groups/groups-loop.php
@@ -21,19 +21,19 @@
 	<?php while ( bp_groups() ) : bp_the_group(); ?>
 		<li>
 			<div class="group-avatar">
-				<a href="<?php bp_group_permalink(); ?>"><?php bp_group_avatar( 'type=thumb&width=50&height=50' ); ?></a>
+				<a href="<?php bp_group_permalink(); ?>"><?php bp_group_avatar( 'type=thumb&width=124&height=124' ); ?></a>
 			</div>
 			<div class="group-content">
 				<div class="group-title"><a href="<?php bp_group_permalink(); ?>"><?php bp_group_name(); ?></a></div>
 				<div class="group-meta"><span class="group-activity"><?php printf( __( 'active %s', 'buddypress' ), bp_get_group_last_active() ); ?></span></div>
 				<div class="group-description"><?php bp_group_description_excerpt(); ?></div>
-				<?php do_action( 'bp_directory_groups_item' ); ?>
-			</div>
-			<div class="group-action">
-				<?php do_action( 'bp_directory_groups_actions' ); ?>
-				<div class="group-meta">
-					<?php bp_group_type(); ?> / <?php bp_group_member_count(); ?>
+				<div class="group-action">
+					<?php do_action( 'bp_directory_groups_actions' ); ?>
+					<div class="group-meta">
+						<?php bp_group_type(); ?> / <?php bp_group_member_count(); ?>
+					</div>
 				</div>
+				<?php do_action( 'bp_directory_groups_item' ); ?>
 			</div>
 		</li>
 	<?php endwhile; ?>
